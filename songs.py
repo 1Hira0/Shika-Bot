30d324e119aeacc1ec0f3ab2937fbfd1059a94ae
Hira 10 <89253787+1Hira0@users.noreply.github.com>
2022-01-25 05:12:51 +0000

    Added gitignore

--- songs.py
+++
@@ -1,3 +0,0 @@
-Fly_Me_To_The_Moon = ["Fly me to the moon", "And let me play among the stars" "Let me see what spring is like", "On Jupiter and Mars", "In other words", "Hold my hand", "In other words", "Darling kiss me"]
-
-Renai_Circulation = ["Se no", "Demo sonnan ja dame", "Mou sonnan ja hora", "Kokoro wa shinka suru yo", "Motto motto"]
