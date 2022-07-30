2aff63fc9722d30e5a71a14c0bea0df309aae6e2
1Hira0 <89253787+1Hira0@users.noreply.github.com>
2022-07-30 23:33:12 +0530

    mistake

--- src/commands/_CommandList.ts
+++
@@ -1,5 +0,0 @@
-import { Command } from "../interfaces/Command";
-import { ping } from "./ping"
-import { jsanime, jsstudio, jschar, jsmanga } from "./anime";
-
-export const CommandList: Command[] = [/*ping,*/ jsanime, jsstudio, jschar, jsmanga];
--- src/utils/validateEnv.ts
+++
@@ -1,6 +0,0 @@
-export const validateEnv = () => {
-    if (!process.env.BOT_TOKEN) {
-      console.warn("Missing Discord bot token.");
-      return false;
-    }
-}
