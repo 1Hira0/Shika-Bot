9d7ab031863a51bf435f67366a52dbcf00b9b1bc
1Hira0 <aryanandaparida86@gmail.com>
2023-09-01 23:08:51 +0530

    Now you know its hash map!

--- src/commands/genshin.ts
+++ src/commands/genshin.ts
@@ -1,6 +1,9 @@
 import fetch from 'node-fetch';
 import { Command } from "../interfaces/Command";
 import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import fs from 'fs';
+
+const characters = JSON.parse(fs.readFileSync('E:\\Hira\\data\\characters.json', 'utf-8'));
 
 export const showcase: Command = {
     data:new SlashCommandBuilder()
@@ -14,7 +17,7 @@ export const showcase: Command = {
                        .setMinValue(100000000)),
     run:async (interaction) => {
         await interaction.deferReply();
-        const response = await fetch(`https://enka.network/api/u/${interaction.options.get('uid')?.value}?info`)
+        const response = await fetch(`https://enka.network/api/uid/${interaction.options.get('uid')?.value}?info`)
         console.log(response)
         if (response.status != 200) {
             await interaction.editReply(`Some error occurred: ${response.status}`) //to add error handling
@@ -25,12 +28,12 @@ export const showcase: Command = {
         const char = playerInfo.showAvatarInfoList
         let dex = ''
         for(let i=0;i<8;i++) {
-            dex = `${dex}${char[i].avatarId}: ${char[i].level}\n`
+            dex = `${dex}${characters[char[i].avatarId].NameTextMapHash}: ${char[i].level}\n`
         }
 
         const embed = new EmbedBuilder()
                             .setTitle(playerInfo.nickname)
-                            .setURL(`https://enka.network/uid/${data.uid}`)
+                            .setURL(`https://enka.network/u/${data.uid}`)
                             .setDescription(dex);
         await interaction.editReply({embeds:[embed]});
     }
