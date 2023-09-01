4cddfade1cfe309fb388846e65879745e838afbd
1Hira0 <aryanandaparida86@gmail.com>
2023-09-01 16:18:38 +0530

    showcase command added

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,5 +1,6 @@
 import { Command } from "../interfaces/Command";
 import { ping } from "./ping"
 import { jsanime, jsstudio, jschar, jsmanga } from "./anime";
+import { showcase } from './genshin';
 
-export const CommandList: Command[] = [/*ping,*/ jsanime, jsstudio, jschar, jsmanga];
+export const CommandList: Command[] = [/*ping,*/ jsanime, jsstudio, jschar, jsmanga, showcase];
