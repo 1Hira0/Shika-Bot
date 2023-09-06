0edfd9ac120e459b701275f5bf8b1cdc3dad87b2
1Hira0 <aryanandaparida86@gmail.com>
2023-09-06 15:18:36 +0530

    Select your ~pokemon~ character

--- src/commands/genshin.ts
+++ src/commands/genshin.ts
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 import { Command } from "../interfaces/Command";
-import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { ActionRowBuilder, ComponentType, EmbedBuilder, SlashCommandBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from 'discord.js';
 import fs from 'fs';
 
 const charDetails = JSON.parse(fs.readFileSync('E:\\Hira\\data\\characters.json', 'utf-8')); //src: https://github.com/EnkaNetwork/API-docs/blob/master/store/characters.json
@@ -17,7 +17,7 @@ export const showcase: Command = {
                        .setMaxValue(999999999)
                        .setMinValue(100000000)),
     run:async (interaction) => {
-        await interaction.deferReply();
+        const msg = await interaction.deferReply();
         const response = await fetch(`https://enka.network/api/uid/${interaction.options.get('uid')?.value}?info`)
         console.log(response)
         if (response.status != 200) {
@@ -28,14 +28,27 @@ export const showcase: Command = {
         const playerInfo = data.playerInfo;
         const char = playerInfo.showAvatarInfoList
         let dex = ''
+        let options = []
         for(let i=0;i<8;i++) {
             dex = `${dex}${charHash[charDetails[char[i].avatarId].NameTextMapHash]}: ${char[i].level}\n`
-        }
+            options.push(new StringSelectMenuOptionBuilder()
+                            .setLabel(charHash[charDetails[char[i].avatarId].NameTextMapHash])
+                            .setValue(`${char[i].avatarId}`))
+        };
 
         const embed = new EmbedBuilder()
                             .setTitle(playerInfo.nickname)
                             .setURL(`https://enka.network/u/${data.uid}`)
                             .setDescription(dex);
-        await interaction.editReply({embeds:[embed]});
+        const select = new StringSelectMenuBuilder()
+            .setCustomId('mommy')
+            .setPlaceholder('Show character')
+            .addOptions(options);
+        const row = new ActionRowBuilder<StringSelectMenuBuilder>()
+            .addComponents(select);
+        await interaction.editReply({embeds:[embed], components:[row]});
+        
+        const collector = msg.createMessageComponentCollector({componentType:ComponentType.StringSelect, time:(15*60)*1000})
+ 
     }
 }
